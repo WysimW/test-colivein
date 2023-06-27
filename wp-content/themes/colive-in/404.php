4c18a3074e3ace08a914e8dfaf5e2c0cf55e06cd
thomas.dupez.vmoclock <dupezthomas@gmail.com>
2023-06-27 18:15:01 +0200

    git pull

---
+++ wp-content/themes/colive-in/404.php
@@ -0,0 +1,16 @@
+<?php
+
+get_header(); ?>
+
+<section class="bg-white">
+
+    <div class="container">
+   <h1>La page ne peut pas être trouvée.</h1>
+   <p>Il semble que rien n’a été trouvé à cet emplacement.</p>
+
+
+    </div>
+</section>
+
+
+<?php get_footer(); ?>
